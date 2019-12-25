b1fa6159e38932a5e8583e23a421b88f30f4d713
Ascencao <rodrigues.mariaelisa@gmail.com>
2019-12-25 19:55:56 -0300

    create component SidebarMain (#17)

--- src/components/organisms/Main/Main.js
+++ src/components/organisms/Main/Main.js
@@ -2,16 +2,14 @@ import React from "react";
 
 import "./Main.css";
 
-import GridHeader from "../GridHeader/GridHeader";
 import SidebarAside from "../SidebarAside/SidebarAside";
+import SidebarMain from "../SidebarMain/SidebarMain";
 
 function Main() {
   return (
     <React.Fragment>
       <div class="lay-sidebar">
-        <div class="sidebar__main">
-          <GridHeader />
-        </div>
+        <SidebarMain />
         <SidebarAside />
       </div>
     </React.Fragment>
---
+++ src/components/organisms/SidebarMain/SidebarMain.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import GridHeader from "../GridHeader/GridHeader";
+
+import "./SidebarMain.css";
+
+function SidebarMain() {
+  return (
+    <React.Fragment>
+      <div class="sidebar__main">
+        <GridHeader />
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default SidebarMain;
