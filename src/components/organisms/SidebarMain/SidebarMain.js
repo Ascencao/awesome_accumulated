4207178d6b95e218dc44a7f940b5ec541ed00f07
Mariaelisa Rodrigues <rodrigues.mariaelisa@gmail.com>
2019-12-26 07:55:08 -0300

    get data from endpoint and order tags

--- src/components/organisms/SidebarMain/SidebarMain.js
+++ src/components/organisms/SidebarMain/SidebarMain.js
@@ -35,8 +35,13 @@ class SidebarMain extends React.Component {
         article => article.subtype === "7"
       );
 
-      this.createCardArticles(articles);
+      this.articles = this.createCardArticles(articles);
+      let tagList = this.getTagList(articles);
+      let groupTags = this.groupTags(tagList);
+      let sortTags = this.orderTags(groupTags);
+      let tagListToRender = this.getTagListToRender(sortTags, tagList);
 
+      this.tagListToRender = tagListToRender.slice(0, 10);
       this.setState({ loading: false, data: articles });
     } catch (error) {
       this.setState({ loading: false, error: error });
@@ -44,7 +49,7 @@ class SidebarMain extends React.Component {
   };
 
   createCardArticles = articles => {
-    this.articles = articles.map((article, id) => {
+    return articles.map((article, id) => {
       return (
         <CardArticle
           key={id}
@@ -57,11 +62,42 @@ class SidebarMain extends React.Component {
     });
   };
 
+  getTagList = articles => {
+    let tagList = articles.map(article => {
+      return article.taxonomy.tags;
+    });
+    return tagList.flat();
+  };
+
+  groupTags = tagList => {
+    let counts = {};
+    tagList.forEach(function(tag) {
+      counts[tag.text] = (counts[tag.text] || 0) + 1;
+    });
+    return counts;
+  };
+
+  orderTags = groupTags => {
+    let orderTags = Object.keys(groupTags).sort((a, b) =>
+      groupTags[a] > groupTags[b] ? -1 : 1
+    );
+    return orderTags;
+  };
+
+  getTagListToRender = (sortTags, tagList) => {
+    return sortTags.map(tagText => {
+      let tag = tagList.find(originalTag => {
+        return originalTag.text === tagText;
+      });
+      return tag;
+    });
+  };
+
   render() {
     return (
       <React.Fragment>
         <div className="sidebar__main">
-          <GridHeader title={headerData.title} labels={headerData.labels} />
+          <GridHeader title={headerData.title} labels={this.tagListToRender} />
           <section className="row-gap-tablet-2 row-gap-deskxl-3 hlp-degrade">
             {this.articles}
           </section>
