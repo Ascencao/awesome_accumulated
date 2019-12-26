4f38cbb825c59c1ef1bb961420e7643824ab0b39
Mariaelisa Rodrigues <rodrigues.mariaelisa@gmail.com>
2019-12-26 00:47:44 -0300

    create component CardArticleDate

--- src/components/organisms/SidebarMain/SidebarMain.js
+++ src/components/organisms/SidebarMain/SidebarMain.js
@@ -4,6 +4,7 @@ import GridHeader from "../GridHeader/GridHeader";
 import headerData from "./header.json";
 import CardArticleImage from "../../atoms/CardArticleImage/CardArticleImage";
 import CardArticleTitle from "../../atoms/CardArticleTitle/CardArticleTitle";
+import CardArticleDate from "../../atoms/CardArticleDate/CardArticleDate";
 
 import "./SidebarMain.css";
 
@@ -12,20 +13,20 @@ function SidebarMain() {
     <React.Fragment>
       <div className="sidebar__main">
         <GridHeader title={headerData.title} labels={headerData.labels} />
-        <section class="row-gap-tablet-2 row-gap-deskxl-3 hlp-degrade">
-          <article class="mod-caja-nota lugares w-100-mobile">
+        <section className="row-gap-tablet-2 row-gap-deskxl-3 hlp-degrade">
+          <article className="mod-caja-nota lugares w-100-mobile">
             <CardArticleImage
               src="https://bucket1.glanacion.com/anexos/fotos/22/3054322h600.jpg"
               alt="Pollo"
               className=""
               url="/"
             ></CardArticleImage>
-            <div class="mod-caja-nota__descrip">
+            <div className="mod-caja-nota__descrip">
               <CardArticleTitle
                 title="La escuela. que tiene de escudo al Che Guevara y donde izan la bandera de Cuba"
                 url="/"
               ></CardArticleTitle>
-              <h4 class="com-date">1 de Julio de 2019</h4>
+              <CardArticleDate date="2019-07-01T12:27:46.485Z"></CardArticleDate>
             </div>
           </article>
         </section>
