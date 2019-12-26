3485a6dc98aa8018784c3f46b899111d0a81800a
Ascencao <rodrigues.mariaelisa@gmail.com>
2019-12-25 22:46:02 -0300

    create component CardArticleImage (#20)

--- src/components/organisms/SidebarMain/SidebarMain.js
+++ src/components/organisms/SidebarMain/SidebarMain.js
@@ -4,12 +4,23 @@ import GridHeader from "../GridHeader/GridHeader";
 import headerData from "./header.json";
 
 import "./SidebarMain.css";
+import CardArticleImage from "../../atoms/CardArticleImage/CardArticleImage";
 
 function SidebarMain() {
   return (
     <React.Fragment>
       <div className="sidebar__main">
         <GridHeader title={headerData.title} labels={headerData.labels} />
+        <section class="row-gap-tablet-2 row-gap-deskxl-3 hlp-degrade">
+          <article class="mod-caja-nota lugares w-100-mobile">
+            <CardArticleImage
+              src="https://bucket1.glanacion.com/anexos/fotos/22/3054322h600.jpg"
+              alt="Pollo"
+              className=""
+              url="/"
+            ></CardArticleImage>
+          </article>
+        </section>
       </div>
     </React.Fragment>
   );
