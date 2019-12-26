ad24f28eaba53564863372cbd4f725d4e6679d95
Mariaelisa Rodrigues <rodrigues.mariaelisa@gmail.com>
2019-12-25 21:35:48 -0300

    Data header json

--- src/components/organisms/SidebarMain/SidebarMain.js
+++ src/components/organisms/SidebarMain/SidebarMain.js
@@ -1,36 +1,11 @@
 import React from "react";
 
 import GridHeader from "../GridHeader/GridHeader";
+import headerData from "./header.json";
 
 import "./SidebarMain.css";
 
 function SidebarMain() {
-  const headerData = {
-    title: "Acumulado Grilla",
-    labels: [
-      {
-        title: "Platos principales",
-        url: "/"
-      },
-      {
-        title: "Cerdo",
-        url: "/"
-      },
-      {
-        title: "Papas",
-        url: "/"
-      },
-      {
-        title: "Date un gustito",
-        url: "/"
-      },
-      {
-        title: "La familia",
-        url: "/"
-      }
-    ]
-  };
-
   return (
     <React.Fragment>
       <div className="sidebar__main">
